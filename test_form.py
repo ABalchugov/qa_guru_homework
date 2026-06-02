22c44dcbb4d3737f697a8db37e13733491372e6c
ABalchugov <inspektr1994@gmail.com>
2026-06-02 21:03:19 +0300

    Refactoring: локаторы выделены в переменные

--- test_form.py
+++ test_form.py
@@ -3,6 +3,13 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+full_name_locator = (By.ID, "userName")
+email_locator = (By.ID, "userEmail")
+current_address_locator = (By.ID, "currentAddress")
+permanent_address_locator = (By.ID, "permanentAddress")
+submit_button_locator = (By.ID, "submit")
+result_box_locator = (By.ID, "output")
+
 def set_up(driver):
     driver.get("https://qa-guru.github.io/one-page-form/text-box.html")
     driver.maximize_window()
@@ -21,22 +28,22 @@ def test_full_name():
         set_up(driver)
 
         # Поиск элементов и заполнение полей
-        full_name_field = driver.find_element(By.ID, "userName")
+        full_name_field = driver.find_element(*full_name_locator)
         full_name_field.send_keys("Александр Александров")
 
         # Находим поле Email по его ID и вводим текст
-        email_field = driver.find_element(By.ID, "userEmail")
+        email_field = driver.find_element(*email_locator)
         email_field.send_keys("alexandr@example.com")
 
         # Находим кнопку Submit по ее ID и кликаем
-        submit_button = driver.find_element(By.ID, "submit")
+        submit_button = driver.find_element(*submit_button_locator)
         submit_button.click()
 
         # Проверка результата
         time.sleep(5)  # Пауза, чтобы увидеть результат отправки
 
         # Находим блок с отправленными данными
-        result_box = driver.find_element(By.ID, "output")
+        result_box = driver.find_element(*result_box_locator)
 
         # Проверяем, что в блоке результата появился введенный текст
         assert "Александр Александров" in result_box.text
@@ -57,22 +64,22 @@ def test_email_negative():
 
         # Поиск элементов и заполнение полей
         # Находим поле Full Name по его ID и вводим текст
-        full_name_field = driver.find_element(By.ID, "userName")
+        full_name_field = driver.find_element(*full_name_locator)
         full_name_field.send_keys("Александр Александров")
 
         # Находим поле Email по его ID и вводим текст
-        email_field = driver.find_element(By.ID, "userEmail")
+        email_field = driver.find_element(*email_locator)
         email_field.send_keys("alexandrexample.com")
 
         # Находим кнопку Submit по ее ID и кликаем
-        submit_button = driver.find_element(By.ID, "submit")
+        submit_button = driver.find_element(*submit_button_locator)
         submit_button.click()
 
         # Проверка результата
         time.sleep(5)  # Пауза, чтобы увидеть результат отправки
 
         # Находим блок с отправленными данными
-        result_box = driver.find_element(By.ID, "output")
+        result_box = driver.find_element(*result_box_locator)
 
         # Проверяем, что в блоке результата появился введенный текст
         assert "alexandrexample.com" not in result_box.text
@@ -93,26 +100,26 @@ def test_current_address():
 
         # Поиск элементов и заполнение полей
         # Находим поле Full Name по его ID и вводим текст
-        full_name_field = driver.find_element(By.ID, "userName")
+        full_name_field = driver.find_element(*full_name_locator)
         full_name_field.send_keys("Александр Александров")
 
         # Находим поле Email по его ID и вводим текст
-        email_field = driver.find_element(By.ID, "userEmail")
+        email_field = driver.find_element(*email_locator)
         email_field.send_keys("alexandr@example.com")
 
         # Находим поле Current Address по его ID и вводим текст
-        current_address_field = driver.find_element(By.ID, "currentAddress")
+        current_address_field = driver.find_element(*current_address_locator)
         current_address_field.send_keys("Москва, Петровка, 38")
 
         # Находим кнопку Submit по ее ID и кликаем
-        submit_button = driver.find_element(By.ID, "submit")
+        submit_button = driver.find_element(*submit_button_locator)
         submit_button.click()
 
         # Проверка результата
         time.sleep(5)  # Пауза, чтобы увидеть результат отправки
 
         # Находим блок с отправленными данными
-        result_box = driver.find_element(By.ID, "output")
+        result_box = driver.find_element(*result_box_locator)
 
         # Проверяем, что в блоке результата появился введенный текст
         assert "Москва, Петровка, 38" in result_box.text
@@ -133,26 +140,26 @@ def test_permanent_address():
 
         # Поиск элементов и заполнение полей
         # Находим поле Full Name по его ID и вводим текст
-        full_name_field = driver.find_element(By.ID, "userName")
+        full_name_field = driver.find_element(*full_name_locator)
         full_name_field.send_keys("Александр Александров")
 
         # Находим поле Email по его ID и вводим текст
-        email_field = driver.find_element(By.ID, "userEmail")
+        email_field = driver.find_element(*email_locator)
         email_field.send_keys("alexandr@example.com")
 
         # Находим поле Permanent Address по его ID и вводим текст
-        permanent_address_field = driver.find_element(By.ID, "permanentAddress")
+        permanent_address_field = driver.find_element(*permanent_address_locator)
         permanent_address_field.send_keys("Москва, Б. Лубянка, 2")
 
         # Находим кнопку Submit по ее ID и кликаем
-        submit_button = driver.find_element(By.ID, "submit")
+        submit_button = driver.find_element(*submit_button_locator)
         submit_button.click()
 
         # Проверка результата
         time.sleep(5)  # Пауза, чтобы увидеть результат отправки
 
         # Находим блок с отправленными данными
-        result_box = driver.find_element(By.ID, "output")
+        result_box = driver.find_element(*result_box_locator)
 
         # Проверяем, что в блоке результата появился введенный текст
         assert "Москва, Б. Лубянка, 2" in result_box.text
