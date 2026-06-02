96645b1cf4b3de7f7a0d8b018c59f5b96396c0fa
ABalchugov <inspektr1994@gmail.com>
2026-06-02 21:46:42 +0300

    Refactoring: создан класс TestSuite

--- test_form.py
+++ test_form.py
@@ -3,173 +3,169 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-full_name_locator = (By.ID, "userName")
-email_locator = (By.ID, "userEmail")
-current_address_locator = (By.ID, "currentAddress")
-permanent_address_locator = (By.ID, "permanentAddress")
-submit_button_locator = (By.ID, "submit")
-result_box_locator = (By.ID, "output")
+class TestSuite:
+    full_name_locator = (By.ID, "userName")
+    email_locator = (By.ID, "userEmail")
+    current_address_locator = (By.ID, "currentAddress")
+    permanent_address_locator = (By.ID, "permanentAddress")
+    submit_button_locator = (By.ID, "submit")
+    result_box_locator = (By.ID, "output")
 
-def set_up(driver):
-    driver.get("https://qa-guru.github.io/one-page-form/text-box.html")
-    driver.maximize_window()
-    time.sleep(5)
+    def __init__(self):
+        self.driver = webdriver.Chrome()
 
-def tear_down(driver):
-    driver.quit()
+    def set_up(self):
+        self.driver.get("https://qa-guru.github.io/one-page-form/text-box.html")
+        self.driver.maximize_window()
+        time.sleep(5)
 
-#Тест поля FullName
-def test_full_name():
-    # Запуск браузера Chrome
-    driver = webdriver.Chrome()
+    def tear_down(self):
+        self.driver.quit()
 
-    try:
-        # Открытие страницы
-        set_up(driver)
+    # Тест поля FullName
+    def test_full_name(self):
 
-        # Поиск элементов и заполнение полей
-        full_name_field = driver.find_element(*full_name_locator)
-        full_name_field.send_keys("Александр Александров")
+        try:
+            # Открытие страницы
+            self.set_up()
 
-        # Находим поле Email по его ID и вводим текст
-        email_field = driver.find_element(*email_locator)
-        email_field.send_keys("alexandr@example.com")
+            # Поиск элементов и заполнение полей
+            full_name_field = self.driver.find_element(*self.full_name_locator)
+            full_name_field.send_keys("Александр Александров")
 
-        # Находим кнопку Submit по ее ID и кликаем
-        submit_button = driver.find_element(*submit_button_locator)
-        submit_button.click()
+            # Находим поле Email по его ID и вводим текст
+            email_field = self.driver.find_element(*self.email_locator)
+            email_field.send_keys("alexandr@example.com")
 
-        # Проверка результата
-        time.sleep(5)  # Пауза, чтобы увидеть результат отправки
+            # Находим кнопку Submit по ее ID и кликаем
+            submit_button = self.driver.find_element(*self.submit_button_locator)
+            submit_button.click()
 
-        # Находим блок с отправленными данными
-        result_box = driver.find_element(*result_box_locator)
+            # Проверка результата
+            time.sleep(5)  # Пауза, чтобы увидеть результат отправки
 
-        # Проверяем, что в блоке результата появился введенный текст
-        assert "Александр Александров" in result_box.text
-        print("Тест поля Full Name успешно пройден!")
+            # Находим блок с отправленными данными
+            result_box = self.driver.find_element(*self.result_box_locator)
 
-    finally:
-        # Закрытие браузера в любом случае
-        tear_down(driver)
+            # Проверяем, что в блоке результата появился введенный текст
+            assert "Александр Александров" in result_box.text
+            print("Тест поля Full Name успешно пройден!")
 
-#Негативный тест поля Email без @
-def test_email_negative():
-    # Запуск браузера Chrome
-    driver = webdriver.Chrome()
+        finally:
+            pass
 
-    try:
-        # Открытие страницы
-        set_up(driver)
+    # Негативный тест поля Email без @
+    def test_email_negative(self):
 
-        # Поиск элементов и заполнение полей
-        # Находим поле Full Name по его ID и вводим текст
-        full_name_field = driver.find_element(*full_name_locator)
-        full_name_field.send_keys("Александр Александров")
+        try:
+            # Открытие страницы
+            self.set_up()
 
-        # Находим поле Email по его ID и вводим текст
-        email_field = driver.find_element(*email_locator)
-        email_field.send_keys("alexandrexample.com")
+            # Поиск элементов и заполнение полей
+            # Находим поле Full Name по его ID и вводим текст
+            full_name_field = self.driver.find_element(*self.full_name_locator)
+            full_name_field.send_keys("Александр Александров")
 
-        # Находим кнопку Submit по ее ID и кликаем
-        submit_button = driver.find_element(*submit_button_locator)
-        submit_button.click()
+            # Находим поле Email по его ID и вводим текст
+            email_field = self.driver.find_element(*self.email_locator)
+            email_field.send_keys("alexandrexample.com")
 
-        # Проверка результата
-        time.sleep(5)  # Пауза, чтобы увидеть результат отправки
+            # Находим кнопку Submit по ее ID и кликаем
+            submit_button = self.driver.find_element(*self.submit_button_locator)
+            submit_button.click()
 
-        # Находим блок с отправленными данными
-        result_box = driver.find_element(*result_box_locator)
+            # Проверка результата
+            time.sleep(5)  # Пауза, чтобы увидеть результат отправки
 
-        # Проверяем, что в блоке результата появился введенный текст
-        assert "alexandrexample.com" not in result_box.text
-        print("Негативный тест поля Email успешно пройден!")
+            # Находим блок с отправленными данными
+            result_box = self.driver.find_element(*self.result_box_locator)
 
-    finally:
-        # Закрытие браузера в любом случае
-        tear_down(driver)
+            # Проверяем, что в блоке результата появился введенный текст
+            assert "alexandrexample.com" not in result_box.text
+            print("Негативный тест поля Email успешно пройден!")
 
-#Тест поля Current Address
-def test_current_address():
-    # Запуск браузера Chrome
-    driver = webdriver.Chrome()
+        finally:
+            pass
 
-    try:
-        # Открытие страницы
-        set_up(driver)
+    # Тест поля Current Address
+    def test_current_address(self):
 
-        # Поиск элементов и заполнение полей
-        # Находим поле Full Name по его ID и вводим текст
-        full_name_field = driver.find_element(*full_name_locator)
-        full_name_field.send_keys("Александр Александров")
+        try:
+            # Открытие страницы
+            self.set_up()
 
-        # Находим поле Email по его ID и вводим текст
-        email_field = driver.find_element(*email_locator)
-        email_field.send_keys("alexandr@example.com")
+            # Поиск элементов и заполнение полей
+            # Находим поле Full Name по его ID и вводим текст
+            full_name_field = self.driver.find_element(*self.full_name_locator)
+            full_name_field.send_keys("Александр Александров")
 
-        # Находим поле Current Address по его ID и вводим текст
-        current_address_field = driver.find_element(*current_address_locator)
-        current_address_field.send_keys("Москва, Петровка, 38")
+            # Находим поле Email по его ID и вводим текст
+            email_field = self.driver.find_element(*self.email_locator)
+            email_field.send_keys("alexandr@example.com")
 
-        # Находим кнопку Submit по ее ID и кликаем
-        submit_button = driver.find_element(*submit_button_locator)
-        submit_button.click()
+            # Находим поле Current Address по его ID и вводим текст
+            current_address_field = self.driver.find_element(*self.current_address_locator)
+            current_address_field.send_keys("Москва, Петровка, 38")
 
-        # Проверка результата
-        time.sleep(5)  # Пауза, чтобы увидеть результат отправки
+            # Находим кнопку Submit по ее ID и кликаем
+            submit_button = self.driver.find_element(*self.submit_button_locator)
+            submit_button.click()
 
-        # Находим блок с отправленными данными
-        result_box = driver.find_element(*result_box_locator)
+            # Проверка результата
+            time.sleep(5)  # Пауза, чтобы увидеть результат отправки
 
-        # Проверяем, что в блоке результата появился введенный текст
-        assert "Москва, Петровка, 38" in result_box.text
-        print("Тест поля Current Address успешно пройден!")
+            # Находим блок с отправленными данными
+            result_box = self.driver.find_element(*self.result_box_locator)
 
-    finally:
-        # Закрытие браузера в любом случае
-        tear_down(driver)
+            # Проверяем, что в блоке результата появился введенный текст
+            assert "Москва, Петровка, 38" in result_box.text
+            print("Тест поля Current Address успешно пройден!")
 
-#Тест поля Permanent Address
-def test_permanent_address():
-    # Запуск браузера Chrome
-    driver = webdriver.Chrome()
+        finally:
+            pass
 
-    try:
-        # Открытие страницы
-        set_up(driver)
+    # Тест поля Permanent Address
+    def test_permanent_address(self):
 
-        # Поиск элементов и заполнение полей
-        # Находим поле Full Name по его ID и вводим текст
-        full_name_field = driver.find_element(*full_name_locator)
-        full_name_field.send_keys("Александр Александров")
+        try:
+            # Открытие страницы
+            self.set_up()
 
-        # Находим поле Email по его ID и вводим текст
-        email_field = driver.find_element(*email_locator)
-        email_field.send_keys("alexandr@example.com")
+            # Поиск элементов и заполнение полей
+            # Находим поле Full Name по его ID и вводим текст
+            full_name_field = self.driver.find_element(*self.full_name_locator)
+            full_name_field.send_keys("Александр Александров")
 
-        # Находим поле Permanent Address по его ID и вводим текст
-        permanent_address_field = driver.find_element(*permanent_address_locator)
-        permanent_address_field.send_keys("Москва, Б. Лубянка, 2")
+            # Находим поле Email по его ID и вводим текст
+            email_field = self.driver.find_element(*self.email_locator)
+            email_field.send_keys("alexandr@example.com")
 
-        # Находим кнопку Submit по ее ID и кликаем
-        submit_button = driver.find_element(*submit_button_locator)
-        submit_button.click()
+            # Находим поле Permanent Address по его ID и вводим текст
+            permanent_address_field = self.driver.find_element(*self.permanent_address_locator)
+            permanent_address_field.send_keys("Москва, Б. Лубянка, 2")
 
-        # Проверка результата
-        time.sleep(5)  # Пауза, чтобы увидеть результат отправки
+            # Находим кнопку Submit по ее ID и кликаем
+            submit_button = self.driver.find_element(*self.submit_button_locator)
+            submit_button.click()
 
-        # Находим блок с отправленными данными
-        result_box = driver.find_element(*result_box_locator)
+            # Проверка результата
+            time.sleep(5)  # Пауза, чтобы увидеть результат отправки
 
-        # Проверяем, что в блоке результата появился введенный текст
-        assert "Москва, Б. Лубянка, 2" in result_box.text
-        print("Тест поля Permanent Address успешно пройден!")
+            # Находим блок с отправленными данными
+            result_box = self.driver.find_element(*self.result_box_locator)
 
-    finally:
-        # Закрытие браузера в любом случае
-        tear_down(driver)
+            # Проверяем, что в блоке результата появился введенный текст
+            assert "Москва, Б. Лубянка, 2" in result_box.text
+            print("Тест поля Permanent Address успешно пройден!")
 
-test_full_name()
-test_email_negative()
-test_current_address()
-test_permanent_address()
+        finally:
+            pass
+
+
+test_suite = TestSuite()
+
+test_suite.test_full_name()
+test_suite.test_email_negative()
+test_suite.test_current_address()
+test_suite.test_permanent_address()
+test_suite.tear_down()
